a879102ae681e2fd7758250087208b44a71410c7
adilfettach <adilerrai24@gmail.com>
2023-08-08 17:19:59 +0100

    this task

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        temp = -(-1*int(abs(number)%10))
+        print(temp)
+    else:
+        temp = number % 10
+        print(temp)
