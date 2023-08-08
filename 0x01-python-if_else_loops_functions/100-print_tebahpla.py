4fb0c5916e75beeb9c6a626fa17f40a84583ac52
adilfettach <adilerrai24@gmail.com>
2023-08-08 20:22:37 +0100

    the task is done

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def funct():
+    for i in reversed(range(65, 91)):
+        if i % 2 == 0:
+            i+=32
+            print(chr(i), end="")
+        else:
+            print(chr(i), end="")
+funct()
