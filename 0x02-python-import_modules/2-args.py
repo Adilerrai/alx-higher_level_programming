2a0e74f6eb7fdf4e9dbcf976a017ffae79523539
adilfettach <adilerrai24@gmail.com>
2023-08-10 17:32:13 +0100

    checked

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv) - 1
-print("{} arguments:".format(n))
-for i in range(1, n+1):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__é=":
+    n = len(sys.argv) - 1
+    print("{} arguments:".format(n))
+    for i in range(1, n+1):
+        print("{}: {}".format(i, sys.argv[i]))
