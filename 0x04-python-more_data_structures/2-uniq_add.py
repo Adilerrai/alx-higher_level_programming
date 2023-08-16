b71600226df5380ed24cc808466ef727735e039f
adilfettach <adilerrai24@gmail.com>
2023-08-16 09:51:37 -0700

    test

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,6 +5,6 @@ def uniq_add(my_list=[]):
     sum = 0
     temp_set = set(my_list)
     temp_list = list(temp_set)
-    for i in range(0, len(temp_list)):
+    for i in range(0, len(temp_list)-1):
         sum += temp_list[i]
         return sum
