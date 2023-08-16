32a33c7fab50da3eeb326d1d5af89438394aaf74
adilfettach <adilerrai24@gmail.com>
2023-08-16 12:10:47 -0700

    simple delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,7 +3,7 @@
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is not None:
         if key is str:
-            del a_dictionary(key)
+            del a_dictionary[key]
         else:
             return None
     else:
