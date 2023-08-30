95f49909e653b16af60f435bf4110028ac3760af
adilfettach <adilerrai24@gmail.com>
2023-08-30 03:45:48 -0700

    tested

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,10 @@
 class Square:
     """Class Object creation"""
     def __init__(self, size):
+         '''Instantiate the Square object with value size
+
+        Args:
+            size: the size argument of the Square object
+        '''
         self.__size = size
 
