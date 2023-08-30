a15c0efc2a46b435027d7954f1b059763e4ccbd9
adilfettach <adilerrai24@gmail.com>
2023-08-30 04:09:34 -0700

    errors raised

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,13 @@
 class Square:
     """Class Object creation"""
     def __init__(self, size=0):
-        """creation of object
+        '''creation of object
 
         Args:
             size (int): The size of the Square object
-            
-        """
-        if not isinstance(size, int)
-            """Check for type"""
+        '''
+        if not isinstance(size, int):
             raise TypeError("TypeError")
         if size < 0:
-            """check for value"""
             raise ValueError("size must be >= 0")
         self.__size = size
