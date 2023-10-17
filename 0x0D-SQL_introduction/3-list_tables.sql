59bedd37d851a8fc908e3d2d583a2054b2970474
adilfettach <adilerrai24@gmail.com>
2023-10-17 13:32:37 +0100

    printign tables of  DB if its exist

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,9 @@
 -- printing all tables of DB 
-SHOW TABLE;
+CREATE PROCEDURE list_tables(database_name VARCHAR(255))
+BEGIN
+	  SELECT table_name
+	  FROM information_schema.tables
+	  WHERE table_schema = database_name;
+END;
+
+CALL list_tables('database_name');
