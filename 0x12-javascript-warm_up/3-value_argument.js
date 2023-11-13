6da27d96b7a7a8753d877d3a6dd1b8290af4250d
Adilfettach <adilerrai24@gmail.com>
2023-11-13 18:33:17 +0100

    checking the array of argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const newList = args.slice(2);
+if (newList[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(newList[0]);
+}
