c03bfc85177b376a09c5317d0b93958ef43e1fc8
Adilfettach <adilerrai24@gmail.com>
2023-11-13 19:48:04 +0100

    printing in order

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const process = require('process');
+const args = process.argv.slice();
+const list = args.slice(2);
+if (list.length === 0) {
+  console.log('undefined is undefined');
+} else if (list.length === 1) {
+  console.log(list[0] + ' is undefined');
+} else if (list.length === 2) {
+  console.log(list[0] + ' is ' + list[1]);
+}
