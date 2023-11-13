012dd1fffc75d376740f44590ea55f8cf15246c2
Adilfettach <adilerrai24@gmail.com>
2023-11-13 21:54:08 +0100

    fixing minor issues

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-const newList = args.slice(2,);
+const newList = args.slice(2);
 const intayger = Math.trunc(Number(newList[0]));
-if(isNaN(intayger)) {
-	console.log('Not a number');
-}else{
-	console.log('My number:' + intayger);
+if (isNaN(intayger)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + intayger);
 }
