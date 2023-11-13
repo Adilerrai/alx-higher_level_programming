b847c73f918f022694d3f80a48d143beb8cbbce1
Adilfettach <adilerrai24@gmail.com>
2023-11-13 22:50:33 +0100

    fixing minor issues

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const NewList = args.slice(2);
+if (isNaN(Number(NewList[0])) || Number(NewList[0]) < 0) {
+  console.log('Missing number of occurrences');
+} else if (Number(NewList[0]) > 0) {
+  let output = '';
+  for (let i = 0; i < Number(NewList[0]); i++) {
+    output += 'X';
+  }
+  console.log(output.trim());
+}
