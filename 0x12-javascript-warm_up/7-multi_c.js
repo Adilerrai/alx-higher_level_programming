0fc654239f77367ce8b9966b0207e0ef2eb94f0e
Adilfettach <adilerrai24@gmail.com>
2023-11-13 22:31:16 +0100

    printing c is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const NewList = args.slice(2);
+if (isNaN(Number(NewList[0]))) {
+  console.log('Missing number of occurrences');
+} else {
+  if (Number(NewList[0]) > 0) {
+    for (let i = 0; i < Number(NewList[0]); i++) {
+      console.log('C is fun');
+    }
+  }
+}
