fdd5833757febb91abd40f2d8d0a4765363d0252
Adilfettach <adilerrai24@gmail.com>
2023-11-14 15:18:19 +0100

    Fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const process = require('process');
+const arg = process.argv[2];
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(arg, 10)));
