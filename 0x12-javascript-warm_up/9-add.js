18ead9dc3a2200d94b4efd1e7646978125226bde
Adilfettach <adilerrai24@gmail.com>
2023-11-14 15:10:09 +0100

    function creation and calling

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const arglist = args.slice(2);
+if (arglist.length > 2) {
+  console.log('NaN');
+} else {
+  const result = add(Number(arglist[0]), Number(arglist[1]));
+  console.log(result);
+}
+
+function add (a, b) {
+  return a + b;
+}
