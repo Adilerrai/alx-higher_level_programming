8586b47ec55f3d978fb483cb7f8dc5782d081739
Adilfettach <adilerrai24@gmail.com>
2023-11-14 15:59:42 +0100

    Fixed this algorithm sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+function swap (arr, firstIndex, secondIndex) {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+}
+function bubbleSortAlgo (arraaytest) {
+  const len = arraaytest.length;
+  let i; let j; let stop;
+  for (i = 0; i < len; i++) {
+    for (j = 0, stop = len - i; j < stop; j++) {
+      if (Number(arraaytest[j]) > Number(arraaytest[j + 1])) {
+        swap(arraaytest, j, j + 1);
+      }
+    }
+  } return arraaytest;
+}
+const process = require('process');
+const args = process.argv;
+const array = args.slice(2);
+
+const sortedArray = bubbleSortAlgo(array);
+
+// Using negative indexing to get the item before the last
+const itemBeforeLast = sortedArray[sortedArray.length - 2];
+console.log(itemBeforeLast);
