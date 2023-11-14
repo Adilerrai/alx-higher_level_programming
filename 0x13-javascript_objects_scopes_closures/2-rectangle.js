8c97ba299f742bded74589fb24f701c5e74cca53
Adilfettach <adilerrai24@gmail.com>
2023-11-14 21:26:30 +0100

    fixing bugs

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-	  if (h>0 && w>0){
-    		this.width = w;
-    		this.height = h;
-  }else{
-  	this.width = 0;
-	this.height = 0;
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 module.exports = Rectangle;
