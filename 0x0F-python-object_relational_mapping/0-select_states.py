ffc5f4e79c8a96ba53f1f368706d2e35e0da215f
Adilfettach <adilerrai24@gmail.com>
2023-11-17 19:44:32 +0100

    testing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ")
+    cur.execute("SELECT * FROM states ORDER BY state.id ")
     [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine()
+
+
+base = declarative_base()
+
+
+class State(base):
+    __tablename__ 
