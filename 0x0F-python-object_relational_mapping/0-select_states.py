46594bd525dccad83377d02f3462d968f9d824a0
Adilfettach <adilerrai24@gmail.com>
2023-11-17 21:41:36 +0100

    changing the importing module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-if __name__ == '__main__':
-    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+
+def list_stat(username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+        Ags:
+                username: mysql username
+                        password: mysql password
+                                database: mysql database
+                                """
+
+    connect = MySQLdb.connect(host="localhost", port=3306, user=username,
+                              passwd=password, db=database)
     cursor = connect.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
@@ -11,3 +20,10 @@ if __name__ == '__main__':
         print(row)
     cursor.close()
     connect.close()
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_stat(username, password, database)
