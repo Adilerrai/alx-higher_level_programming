df5708e8839c1410e3e3a0e5506195718a680f85
Adilfettach <adilerrai24@gmail.com>
2023-11-17 19:13:40 +0100

    checking the length of the sysargv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,14 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ")
     [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
