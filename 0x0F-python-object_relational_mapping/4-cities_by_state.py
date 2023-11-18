4306e8fd5e89d47debf03612f3fa0dab4b1c4a43
Adilfettach <adilerrai24@gmail.com>
2023-11-18 14:55:57 +0100

    testing

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,38 +5,36 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-        # Get MySQL connection parameters from command line arguments
-        username, password, database = sys.argv[1:]
+    # Get MySQL connection parameters from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-        # Connect to MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=username, passwd=password, db=database)
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
 
-        # Create a cursor object using the cursor() method
-        cursor = db.cursor()
+    # Create a cursor object using the cursor() method
+    cursor = db.cursor()
 
-        # Execute a single query to retrieve the required data
-        cursor.execute(
-            """
-                SELECT cities.id, cities.name, states.na
+    # Execute a single query to retrieve the required data
+    cursor.execute(
+        """
+                SELECT cities.id, cities.name, states.name
                 FROM cities
                 JOIN states ON cities.state_id = states.id
                 ORDER BY cities.id ASC;
                 """
-        )
+    )
 
-        # Fetch all the rows using the fetchall() method
-        rows = cursor.fetchall()
+    # Fetch all the rows using the fetchall() method
+    rows = cursor.fetchall()
 
-        # Display the results as specified
-        for row in rows:
-            print(row)
+    # Display the results as specified
+    for row in rows:
+        print(row)
 
-            # Close the cursor and database connection
-        cursor.close()
-        db.close()
+        # Close the cursor and database connection
+    cursor.close()
+    db.close()
