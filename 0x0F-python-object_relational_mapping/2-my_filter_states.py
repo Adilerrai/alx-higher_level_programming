d6fd05f0f60cbd30c47ab8baf764c31347b0d3f9
Adilfettach <adilerrai24@gmail.com>
2023-11-18 14:19:10 +0100

    fixing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
-"""Script that list all state """
-import MySQLdb
+""" List all states from hbtn_0e_0_usa """
 import sys
-
-
-def list_stat(username, password, database, userInput):
-    """lists all states from the database hbtn_0e_0_usa.
-    Ags:
-    username: mysql username
-    password: mysql password
-    database: mysql database
-    """
-
-    connect = MySQLdb.connect(host="localhost", port=3306, user=username,
-                              passwd=password, db=database)
-    cursor = connect.cursor()
-    name = userInput
-    query = "SELECT * FROM states WHERE name = %s"
-    cursor.execute(query, (name,))
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    connect.close()
-
+import MySQLdb
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    userinput = sys.argv[4]
-    list_stat(username, password, database, userinput)
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+            WHERE name = '{}' ORDER BY id".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
