297178278c7a5caecf1e4ae49b8ac019a05db343
Adilfettach <adilerrai24@gmail.com>
2023-11-18 19:10:27 +0100

    Commiting first changes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+""" Define the state model """
 
-from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 
 class State(Base):
-    """Doing something"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    """State class inheriting from Base class
+
+        Args:
+                __tablename__(str): The name of the table
+                        id(int)           : The table's primary key
+                                name(str)         : The state name
+                                    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
