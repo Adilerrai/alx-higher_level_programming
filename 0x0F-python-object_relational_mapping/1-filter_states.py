60b14409a5ba517a412a57dee31f38eb60a0ffe5
Adilfettach <adilerrai24@gmail.com>
2023-11-18 13:45:08 +0100

    listing like a schema

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Script that list all state """
+import MySQLdb
+import sys
+
+
+def list_stat(username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+    username: mysql username
+    password: mysql password
+    database: mysql database
+    """
+
+    connect = MySQLdb.connect(host="localhost", port=3306, user=username,
+                              passwd=password, db=database)
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states like N% ORDER  BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connect.close()
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_stat(username, password, database)
