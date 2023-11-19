dec12feb9525958fb9f9336ac7e4cadb55eaf925
Adilfettach <adilerrai24@gmail.com>
2023-11-19 17:16:32 +0100

    Quering for the first elem

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine,
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sys
