ce16d221ef13337e7ad6cd6fc3c6ae760e82bb20
Adilfettach <adilerrai24@gmail.com>
2023-11-19 18:39:57 +0100

    trying and fixing

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""This is a script """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State.id == 2).first()
+    if state:
+        state.name = 'New Mexico'
+
+    session.add(state)
+    session.commit()
+    session.close()
