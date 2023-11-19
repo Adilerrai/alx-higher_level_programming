07882e4a595d5c2a37939969272dade54b902c1f
Adilfettach <adilerrai24@gmail.com>
2023-11-19 17:54:31 +0100

    querying like

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""This is a script """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')
+    session.close()
