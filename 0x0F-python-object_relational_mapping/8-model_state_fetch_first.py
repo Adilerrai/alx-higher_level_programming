fe6cb404121f890a989d8ef114b2c0a8772073cc
Adilfettach <adilerrai24@gmail.com>
2023-11-19 17:14:26 +0100

    Quering for the first elem

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine, String, Integer, Column
+from sqlalchemy import create_engine,
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sys
-from model_state import Base, State
+from model_state import State
 
 
 if __name__ == '__main__':
@@ -14,10 +14,9 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
-    if len(states) == 0:
-        print('Nothing')
-    else:
+    states = session.query(State).order_by(State.id).first()
+    if states:
         print("{}: {}".format(state[0].id, state[0].name))
-
+    else:
+        print('Nothing')
     session.close()
