8bdf8bdb67d175420f96ffe6ae0518474dbca58e
Adilfettach <adilerrai24@gmail.com>
2023-11-19 17:25:03 +0100

    fixing syntax error

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).first()
+    states = session.query(State).filter(State.id).first()
     if states:
         print("{}: {}".format(states.id, states.name))
     else:
