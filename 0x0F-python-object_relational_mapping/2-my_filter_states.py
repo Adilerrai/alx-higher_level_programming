19487ac1b78803e27df13ec50e7080a8f018c2e4
Adilfettach <adilerrai24@gmail.com>
2023-11-20 13:33:10 +0100

    fixing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     pattern = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name\
-            REGEXP CONCAT('^', 'Nevada') COLLATE utf8mb4_bin ORDER BY id ASC;")
+    query = "SELECT * FROM states WHERE name REGEXP CONCAT('^', %s) COLLATE utf8mb4_bin ORDER BY id ASC;"
+    cur.execute(query, (pattern,))
+
     [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
