3e685ddda12b9b56e6593c6a5792904fe038f70d
Adilfettach <adilerrai24@gmail.com>
2023-11-30 14:41:26 +0100

    tested

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this  script will send request to the server and retrieve  
-curl -s -sI "$1" | grep 'Content-length'|awk '{print $2}'
+curl -s -sI "$1" | grep -i 'Content-length'|awk '{print $2}'
