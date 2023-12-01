8e489609df094a5e818705208ad0c3eee920842c
adilfettach <adilerrai24@gmail.com>
2023-12-01 02:38:37 -0800

    trying to print headers items

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Creating something """
+import sys
+from urllib.request import urlopen
+with urlopen(sys.argv[1]) as response:
+        body = response
+
+header = body.__dict__['headers']
+for key, val in header.items():
+    if key == 'X-Request-Id':
+        print(val)
