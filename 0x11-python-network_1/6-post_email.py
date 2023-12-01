214a6a132f2e587e626beafc96fb7e3b1ac73d72
adilfettach <adilerrai24@gmail.com>
2023-12-01 14:01:09 -0800

    fixing

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Creating something"""
+
+import sys
+import requests
+import json
+
+url = sys.argv[1]
+
+data = {"email": sys.argv[2]}
+
+r = requests.post(url, json=data)
+
+print(r.text)
