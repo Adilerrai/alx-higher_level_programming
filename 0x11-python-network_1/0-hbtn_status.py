34239d1a83053a49e2b025196b248f6fe741d758
adilfettach <adilerrai24@gmail.com>
2023-12-01 03:09:19 -0800

    fixing importation issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """Script see the body response """
 
 from urllib.request import urlopen
-with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-print("Body response:")
-print(f"    - type: {body.__class__}")
-print(f"    - content: {body}")
-print(f"    - utf8 content: {body.decode('utf-8')}")
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+    print("Body response:")
+    print(f"    - type: {body.__class__}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,14 @@
 """Creating something """
 import sys
 from urllib.request import urlopen
-with urlopen(sys.argv[1]) as response:
-    x_request_id = response.headers.get('X-Request-Id')
 
-print(x_request_id)
+
+def fetching_ud():
+
+    with urlopen(sys.argv[1]) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
+
+
+if __name__ == "__main__":
+    fetching_ud()
