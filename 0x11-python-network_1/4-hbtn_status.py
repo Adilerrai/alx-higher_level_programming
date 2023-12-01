a84dc44b4027c7aafb8d8d6743e6bf7046605531
adilfettach <adilerrai24@gmail.com>
2023-12-01 09:50:31 -0800

    fixing tabulation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Creating something"""
 
-import requests 
+import requests
+
 if __name__ == "__main__":
     url = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("    - type:", type(url.text))
-    print("    - content:", url.text)
+    data = url.text
+    resType = type(data)
+    print(f"Body response:\n\t- type: {resType}\n\t- content: {data}")
