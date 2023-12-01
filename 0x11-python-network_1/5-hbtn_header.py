1cf9e911f386effbb2b551f862e288d45d3568d1
adilfettach <adilerrai24@gmail.com>
2023-12-01 09:56:00 -0800

    fixing this

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Doc"""
+
+import sys 
+import requests
+if __name__ == "__main__":
+    url = requests.get(sys.argv[1])
+    data = url.headers.get("X-Request-Id")
+
+    print(data)
