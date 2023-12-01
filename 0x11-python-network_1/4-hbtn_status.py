f58e13feed19b240d87b52fe1dd979f7efff8adc
adilfettach <adilerrai24@gmail.com>
2023-12-01 09:33:33 -0800

    fixing

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+import requests 
+if __name__ == "__main__":
+    url = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("    - type:", type(url.text))
+    print("    - content:", url.text)
