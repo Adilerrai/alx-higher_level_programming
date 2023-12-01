6f86a33cbc70f005e0e05878c68513b601dbedb8
adilfettach <adilerrai24@gmail.com>
2023-12-01 14:44:02 -0800

    fixing

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,7 +9,4 @@ if __name__ == "__main__":
     email = sys.argv[2]
     data = {"email": email}
     r = requests.post(url, data=data)
-    if r.text >= 400:
-        print(f"Error code: {r.text}")
-    else:
-        print(r.text)
+    print(r.text)
