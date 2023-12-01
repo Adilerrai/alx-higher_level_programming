7cdd89df3bdd838f34dc2b5f71977ac1d1efa6f0
adilfettach <adilerrai24@gmail.com>
2023-12-01 14:19:44 -0800

    posting someting

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,10 +5,9 @@ import sys
 import requests
 import json
 
-url = sys.argv[1]
-
-data = {"email": sys.argv[2]}
-
-r = requests.post(url, json=data)
-
-print(r.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    r = requests.post(url, data=data)
+    print(r.text)
