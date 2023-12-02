7cf0c8ac4208d766fc0fb68caeb3e53e0c6e7c82
Adilfettach <adilerrai24@gmail.com>
2023-12-02 10:47:56 +0100

    fixing errors code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Documentated """
+from requests import request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = request.get(url)
+    if req.stats_code >= 400:
+        print(f"Error code: {req.status_code}")
+    else:
+        print("index")
